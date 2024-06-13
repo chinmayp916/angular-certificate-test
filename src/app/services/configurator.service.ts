d7a77d3ebae938333675c34f1d300d916a3b68c1
chinmayp916 <Chinu0916@gmail.com>
2024-06-13 20:09:14 +0530

    added changes of clase

--- src/app/services/configurator.service.ts
+++ src/app/services/configurator.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Model } from '../model/Model';
+import { Color } from '../model/Color';
+import { Config } from '../model/Config';
+import { SelectedCar } from '../model/SelectedCar';
+import { Option } from '../model/Option';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfiguratorService {
-  public selectedModel: any;
-  public selectedModelCode: any;
-  public selectedColor: any;
-  public selectedImage: any;
-  public selectedConfig: any;
+  public selectedModel: Model | null = null;
+  public selectedModelCode: Option | null = null;
+  public selectedColor: Color | null = null;
+  public selectedImage: SelectedCar | null = null;
+  public selectedConfig: Config | null = null;
   public yoke: boolean = false;
   public towHitch: boolean = false;
 
